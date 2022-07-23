7e9a17686903cbb8b2fa829fd94b073d5896c000
CamilovgTeleco <dfgusp@gmail.com>
2022-07-23 11:04:54 -0500

    Se integró el código del archivo clase6.html. Se hizo maquetación de HTML desde JS con el fin de tener una lista de productos más responsive.

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const menuMobile= document.querySelector('.mobile-menu')
 const btnShoppingCart = document.querySelector('.navbar-shopping-cart')
 const productDetailShopCar = document.querySelector('.product-detail')
 
+const cardsContainer = document.querySelector('.cards-container')
+
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcom.addEventListener('click', toggleBtnHamburguesa)
 btnShoppingCart.addEventListener('click', toggleBtnShopCar)
@@ -66,6 +68,66 @@ function toggleBtnShopCar(){
        desktopMenu.classList.add('inactive')
     }
 }
+
+/*Integración código clase6.html*/ 
+
+const productList=[];
+
+productList.push({
+    name:"Bike",
+    price:120, 
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+productList.push({
+    name:"Pantalla",
+    price:220, 
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+productList.push({
+    name:"Computador",
+    price:500, 
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+for (product of productList){
+    const productCard= document.createElement('div')
+    productCard.classList.add('product-card')
+
+    const img = document.createElement('img')
+    img.setAttribute('src',product.image)
+
+    const productInf= document.createElement('div')
+    productInf.classList.add('product-info')
+
+
+    const productInfoDiv=document.createElement('div')
+    
+    const productPrice=document.createElement('p')
+    productPrice.innerHTML = '$' + product.price
+    
+
+    const productName=document.createElement('p')
+    productName.innerText=product.name
+    
+    const productInfoFigure=document.createElement('figure')
+    
+    const productImgCard=document.createElement('img')
+    productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+    productInfoFigure.append(productImgCard)
+    productInfoDiv.append(productPrice)
+    productInfoDiv.append(productName)
+    productInf.append(productInfoDiv)
+
+    productCard.append(img)
+    productCard.append(productInf)
+    productInf.append(productInfoFigure)
+
+    cardsContainer.append(productCard)
+}
+
 
 
 
